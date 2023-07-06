1bad11a864fd36c1fdeb86a5893c7e8af3d5a989
Iann Rodrigues <iannrst11@gmail.com>
2023-07-06 16:01:52 -0300

    feat: Config fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import { Baloo2_700Bold, useFonts } from '@expo-google-fonts/baloo-2';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Baloo2_700Bold,
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if(!fontsLoaded){
+    return null
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <>
+    <StatusBar 
+      backgroundColor="transparent"
+      translucent 
+      barStyle="light-content"
+    />
+    
+    <Home />
+  </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
