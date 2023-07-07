4c117fa05cfb560e0eba2977c5c1224171e699cd
Iann Rodrigues <iannrst11@gmail.com>
2023-07-07 16:33:15 -0300

    feat: add componente de Loading

--- App.tsx
+++ App.tsx
@@ -3,17 +3,20 @@ import { StatusBar } from 'react-native';
 import { Baloo2_700Bold, useFonts } from '@expo-google-fonts/baloo-2';
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Baloo2_700Bold,
     Roboto_400Regular,
     Roboto_700Bold
   })
 
   if(!fontsLoaded){
-    return null
+    return (
+      <Loading />
+    )
   }
   
   return (
