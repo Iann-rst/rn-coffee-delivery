7d3c787cbae7a45ce9d2d6cba642103bd98ec96a
Iann Rodrigues <iannrst11@gmail.com>
2023-07-11 18:29:54 -0300

    feat: Adicionando outras telas de navegação

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,12 +1,27 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Home } from '../screens/Home'
+import { NativeStackNavigationProp, createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Cart } from '../screens/Cart';
+import { Finish } from '../screens/Finish';
+import { Home } from '../screens/Home';
+import { Product } from '../screens/Product';
 
-const {Navigator, Screen} = createNativeStackNavigator()
+type RootStackParamList = {
+  home: undefined;
+  product: {productId: number};
+  cart: undefined;
+  finish: undefined;
+}
+
+export type AppNavigatorRoutesProps = NativeStackNavigationProp<RootStackParamList>
+
+const {Navigator, Screen} = createNativeStackNavigator<RootStackParamList>()
 
 export function AppRoutes(){
   return(
     <Navigator screenOptions={{headerShown: false}}>
       <Screen name='home' component={Home}/>
+      <Screen name='product' component={Product}/>
+      <Screen name='cart' component={Cart}/>
+      <Screen name='finish' component={Finish}/>
     </Navigator>
   )
 }
