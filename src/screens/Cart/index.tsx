1915e84e17081f1fd51c5eaa538a1ccd758b0ef5
Iann Rodrigues <iannrst11@gmail.com>
2023-07-11 23:44:24 -0300

    feat: Tela do carrinho de compras

---
+++ src/screens/Cart/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Cart(){
+  return(
+    <View style={styles.container}>
+      <Text>Carrinho de Compras</Text>
+    </View>
+  )
+}
