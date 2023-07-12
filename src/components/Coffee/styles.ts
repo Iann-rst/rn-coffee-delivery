fa9290bc0207092bb917ab82ac10c6f1e8b0414a
Iann Rodrigues <iannrst11@gmail.com>
2023-07-12 17:25:23 -0300

    feat: Componente animado da xícara de café

---
+++ src/components/Coffee/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    marginTop: 62,
+    marginBottom: -62,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+
+  canvas: {
+    width: 64,
+    height: 335,
+    position: 'absolute',
+    zIndex: 1
+  }
+})
