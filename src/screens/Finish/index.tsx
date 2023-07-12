71a418c53a093b2d917a5e6622e5ee0a7116d031
Iann Rodrigues <iannrst11@gmail.com>
2023-07-11 23:44:49 -0300

    feat: Tela de pedido finalizado

---
+++ src/screens/Finish/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Finish(){
+  return(
+    <View style={styles.container}>
+      <Text>Finish - Compra finalizada</Text>
+    </View>
+  )
+}
