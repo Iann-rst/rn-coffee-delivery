276cbb3115facd607d54e32e1b0caa8df0a3e6e6
Iann Rodrigues <iannrst11@gmail.com>
2023-07-13 18:01:52 -0300

    feat: Implementando a tela do carrinho

--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,10 +1,124 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { ArrowLeft } from "phosphor-react-native";
+import { useEffect, useMemo, useState } from "react";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { AppNavigatorRoutesProps } from "../../routes/app.routes";
+
+
+import { Loading } from "../../components/Loading";
+
+import { useCart } from "../../hooks/useCart";
+import { CoffeeProps } from "../../storage/storageCart";
+
+import Animated, { Layout, SlideInRight, SlideOutRight } from "react-native-reanimated";
+import { Button } from "../../components/Button";
+import { CoffeeCartCard } from "../../components/CoffeeCartCard";
+import { EmptyCart } from "../../components/EmptyCart";
+import { THEME } from "../../styles/theme";
 import { styles } from "./styles";
 
 export function Cart(){
+  const {fetchCoffeesCart, removeCoffeeCart} = useCart()
+  const {navigate} = useNavigation<AppNavigatorRoutesProps>()
+
+  const [coffees, setCoffees] = useState<CoffeeProps[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  
+  const amount = useMemo(() => {
+    return coffees.reduce((accumulator, item) => {
+      return accumulator + (item.price * item.qtd);
+    }, 0);
+  }, [coffees]);
+
+  async function fetchData(){
+    try {
+      const data = await fetchCoffeesCart() 
+      setCoffees(data)
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setIsLoading(false)
+    }
+  }
+
+  async function handleRemove(id: string){
+    await removeCoffeeCart(id)
+    await fetchData()   
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
+
+  if(isLoading){
+    return <Loading />
+  }
+
   return(
     <View style={styles.container}>
-      <Text>Carrinho de Compras</Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={()=>navigate("home")}>
+          <ArrowLeft size={24} color={THEME.COLORS.GRAY_100}/>
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Carrinho</Text>
+        <View />
+      </View>
+      
+     {
+      coffees.length===0 ? (
+        <EmptyCart />
+      ): (
+        <>
+          <ScrollView
+            style={{flex: 1}}
+            contentContainerStyle={{flexGrow: 1, paddingBottom: 120}}
+            showsVerticalScrollIndicator={false}
+          >
+            {
+              coffees.map((coffee, index)=> (
+                <Animated.View
+                  style={{
+                    borderBottomWidth: 1,
+                    borderBottomColor: THEME.COLORS.GRAY_700
+                  }}
+                  key={coffee.id} 
+                  entering={SlideInRight.delay(index*100)} 
+                  exiting={SlideOutRight} 
+                  layout={Layout.springify()}
+                >
+                  <CoffeeCartCard 
+                    coffee={coffee} 
+                    remove={()=>handleRemove(coffee.id)}
+                  />
+                </Animated.View>
+             ))
+            }
+          </ScrollView>
+          <View style={styles.footer}>
+            <View style={{
+              flexDirection: 'row', 
+              alignItems: 'center', 
+              justifyContent: 'space-between'
+            }}>
+              <Text style={styles.amount}>
+                Valor total
+              </Text>
+            
+              <Text style={styles.price}>
+                R$ {amount.toFixed(2)}
+              </Text>
+            </View>
+            <Button 
+              title="Confirmar pedido" 
+              type="YELLOW" 
+              activeOpacity={0.6}
+            />
+          </View>
+        </>
+      )
+     }
     </View>
   )
 }
--- src/screens/Cart/styles.ts
+++ src/screens/Cart/styles.ts
@@ -1,5 +1,56 @@
 import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: THEME.COLORS.GRAY_900,
+    paddingTop: 18,
+  },
+
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: THEME.COLORS.GRAY_700,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    paddingHorizontal: 32,
+    paddingVertical: 26
+  },
+
+  title: {
+    fontFamily: THEME.FONTS.TITLE_BOLD,
+    fontSize: THEME.SIZES.TITLE_SM,
+    color: THEME.COLORS.GRAY_200
+  },
+
+  footer: {
+    width: '100%',
+    backgroundColor: THEME.COLORS.WHITE,
+    gap: 20,
+    paddingHorizontal: 32,
+    paddingTop: 28,
+    paddingBottom: 40,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: -2
+    },
+    shadowOpacity: 0.08,
+    shadowRadius: 16,
+    elevation: 10
+  },
+
+  amount: {
+    fontFamily: THEME.FONTS.REGULAR,
+    fontSize: THEME.SIZES.TEXT_MD,
+    color: THEME.COLORS.GRAY_200
+  },
+
+  price: {
+    fontFamily: THEME.FONTS.TITLE_BOLD,
+    fontSize: THEME.SIZES.TEXT_MD,
+    color: THEME.COLORS.GRAY_200
+  }
 })
