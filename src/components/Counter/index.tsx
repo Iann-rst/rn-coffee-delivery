3abd5571416f211fd1e6f3609462c557f4ca3998
Iann Rodrigues <iannrst11@gmail.com>
2023-07-13 18:01:02 -0300

    feat: Adicionando borda na variante secundária

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -8,11 +8,20 @@ type Props = {
   increment: ()=>void;
   decrement: ()=>void;
   count: number
+  variant?: 'primary' | 'secondary' 
 }
 
-export function Counter({count, decrement, increment}:Props){
+export function Counter({count, variant = 'primary', decrement, increment}:Props){
+  const styleSecondary = {
+    borderWidth: 1,
+    borderColor: THEME.COLORS.GRAY_600,
+    borderRadius: 6,
+  }
   return(
-    <View style={styles.container}>
+    <View style={[
+      styles.container, 
+      variant==='secondary' && styleSecondary
+    ]}>
       <Pressable style={styles.button} onPress={decrement}>
         <Minus size={20} weight="bold" color={THEME.COLORS.PURPLE}/>
       </Pressable>
--- src/components/Counter/styles.ts
+++ src/components/Counter/styles.ts
@@ -5,6 +5,7 @@ export const styles = StyleSheet.create({
   container: {
     width: 100,
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
     gap: 4
   },
