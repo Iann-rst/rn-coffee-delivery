337dc73069f7d9edc28dcdd22fe6810662b78859
Iann Rodrigues <iannrst11@gmail.com>
2023-07-14 15:22:03 -0300

    feat: Finalizando tela de pedido confirmado

--- src/screens/Finish/index.tsx
+++ src/screens/Finish/index.tsx
@@ -1,10 +1,96 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { Dimensions, Text, View } from "react-native";
+import Animated, { Keyframe, SlideInLeft, runOnJS } from "react-native-reanimated";
+
+import { Button } from "../../components/Button";
+import { AppNavigatorRoutesProps } from "../../routes/app.routes";
+import { cartRemoveAll } from "../../storage/storageCart";
+
+import MotoSvg from '../../assets/delivery.svg';
 import { styles } from "./styles";
 
+
+const SCREEN_WIDTH = Dimensions.get('window').width
+
 export function Finish(){
+  const [visible, setVisible] = useState(false)
+  
+  const {navigate} = useNavigation<AppNavigatorRoutesProps>()
+
+  const enteringKeyframeSVG = new Keyframe({
+    0: {
+      opacity: 0,
+      transform: [
+        {translateX: SCREEN_WIDTH * -1},
+      ]
+    },
+    70: {
+      opacity: 0.3
+    },
+    100: {
+      opacity: 1,
+      transform: [
+        {translateX: 0}
+      ]
+    }
+  })
+
+  const enteringKeyframe = new Keyframe({
+    0: {
+      opacity: 0,
+      transform: [
+        {translateX: SCREEN_WIDTH * -1},
+      ]
+    },
+    70: {
+      opacity: 0.3
+    },
+    100: {
+      opacity: 1,
+      transform: [
+        {translateX: 0}
+      ]
+    }
+  })
+
+  async function handleFinish(){
+    try {
+      await cartRemoveAll()
+      navigate("home")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return(
     <View style={styles.container}>
-      <Text>Finish - Compra finalizada</Text>
+      <View style={{width: '100%'}}>
+        <Animated.View entering={enteringKeyframeSVG.duration(600)}>
+          <MotoSvg />
+        </Animated.View>
+        <Animated.View entering={enteringKeyframe.delay(600).duration(800).withCallback((finished)=> {
+          'worklet';
+          if(finished){
+            runOnJS(setVisible)(true)
+          }
+        })}>
+
+          <Text style={styles.title}>Uhu! Pedido confirmado</Text>
+          <Text style={styles.subTitle}>Agora é só aguardar que logo o café chegará até você!</Text>
+        </Animated.View>
+        
+        {
+        visible && (
+          <Animated.View entering={SlideInLeft.duration(300)}>
+            <Button 
+              title="ir para a home" 
+              type="PURPLE"
+              onPress={handleFinish}
+            />
+          </Animated.View>
+        )}
+      </View>
     </View>
   )
 }
--- src/screens/Finish/styles.ts
+++ src/screens/Finish/styles.ts
@@ -1,5 +1,28 @@
 import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: THEME.COLORS.GRAY_900,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 64
+  },
+  
+  title: {
+    marginTop: 44,
+    fontFamily: THEME.FONTS.BOLD,
+    fontSize: THEME.SIZES.TITLE_LG,
+    color: THEME.COLORS.YELLOW_DARK,
+    textAlign: 'center'
+  },
+  subTitle: {
+    marginTop: 8,
+    marginBottom: 64,
+    textAlign: 'center',
+    color: THEME.COLORS.GRAY_200,
+    fontFamily: THEME.FONTS.REGULAR,
+    fontSize: THEME.SIZES.TEXT_SM
+  }
 })
